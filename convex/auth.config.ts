08b13a96c169fa0ac0a70e174423f3fd3625d647
Faya1495 <faye43480@gmail.com>
2024-03-11 23:04:02 +0300

    firstDeploy

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+export default {
+  providers: [
+    {
+      domain: "https://golden-frog-94.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+//https://golden-frog-94.clerk.accounts.dev
